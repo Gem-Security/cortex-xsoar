58c482c41bc400b22475778bd1ab6f294a1c198b
Lior Maman <lior@gem.security>
2024-03-04 16:13:04 +0000

    Fix script

--- Packs/Gem/Scripts/ResolveGemThreat/ResolveGemThreat.py
+++ Packs/Gem/Scripts/ResolveGemThreat/ResolveGemThreat.py
@@ -3,21 +3,23 @@ from CommonServerPython import *  # noqa: F401
 
 def main():
     incident = demisto.incident()
-    incident.get("closeReason", "")
+    # incident.get("closeReason", "")
     close_notes = incident.get("closeNotes", "")
     incident_id = incident.get("id", "")
-    threat_id = incident.get("CustomFields").get("threatid")
-    verdict = incident.get("CustomFields").get("verdict")
-    verdict = verdict if verdict != "" else "inconclusive"
+    threat_id = incident.get("CustomFields").get("gemthreatid")
+    verdict = incident.get("CustomFields").get("gemverdict")
+    verdict = verdict if verdict else "inconclusive"
     verdict = verdict.replace(" ", "_").lower()
     status = "resolved"
 
+    demisto.log(f"Resolving Gem Threat {threat_id} with status {status}, verdict {verdict} and close notes {close_notes}")
     demisto.executeCommand("gem-update-threat-status", {
         "verdict": verdict,
         "reason": f"Closed from XSOAR, incident id: {incident_id}\n"
         f"\nClose Notes:\n{close_notes}",
         "threat_id": threat_id,
         "status": status})
+
     demisto.log(f"Resolved Gem Threat {threat_id} with status {status}, verdict {verdict} and close notes {close_notes}")
 
 
