f32942a236663fdd912336f04addeccd971b207a
Lior Maman <lior.maman@wiz.io>
2024-05-06 14:39:20 +0000

    fix

--- Packs/Gem/Integrations/Gem/Gem.py
+++ Packs/Gem/Integrations/Gem/Gem.py
@@ -575,8 +575,8 @@ def init_client(params: dict) -> GemClient:
         base_url=params['api_endpoint'],
         verify=True,
         proxy=params.get('proxy', False),
-        client_id=demisto.getParam('credentials')['identifier'], # type: ignore
-        client_secret=demisto.getParam('credentials')['password'] # type: ignore
+        client_id=demisto.getParam('credentials', {})['identifier'], # type: ignore
+        client_secret=demisto.getParam('credentials', {})['password'] # type: ignore
     )
 
 
